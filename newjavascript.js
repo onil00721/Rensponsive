9f4c921c495ab17ede4c7c83f3489a23fe7c2939
onil00721 <35064963+onil00721@users.noreply.github.com>
2018-01-17 11:59:49 +0100

    11,59

--- newjavascript.js
+++ newjavascript.js
@@ -1,6 +1,19 @@
 document.write("fsd");
+console.log("dfgdfgdfgdfgdfg");
+$(".menuBox").click(function(){
+
+    $(".podMenu").css("display" , "block" );
+   
+
+});
 
 
+
+
+
+/*$(".menuBox").mousemove(function(){
+    $("podMenu").css({"display": "block";})
+});*/
 
 
               
